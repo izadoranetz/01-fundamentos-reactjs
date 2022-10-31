5cb25e102d06d621c630e6717c4f4405e098e1c9
izadoranetz <izadora.ns@gmail.com>
2022-10-30 22:48:52 -0300

    criando projeto

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Post } from './Post';
+
+export function App() {
+  return (
+    <div>
+      <Post
+        author="Izadora andorinea"
+        content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas, quisquam eius, aliquid aut ut, fugit eveniet ea voluptates assumenda temporibus accusamus iste enim quidem fugiat. Molestiae rerum officiis aliquid quas?"
+      />
+      <Post 
+              author="Cacau gatineo"
+              content="Birman manx lynx yet cheetah. Cornish rex turkish angora malkin. Abyssinian american bobtail or thai or siberian, but himalayan but scottish fold and lynx. Himalayan."
+      
+      />
+    </div>
+  );
+}
