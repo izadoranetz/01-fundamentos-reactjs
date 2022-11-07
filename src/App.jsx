c196854cd46a89fa08a48deb805d61bbf0819a4c
izadoranetz <izadora.ns@gmail.com>
2022-11-07 19:32:58 -0300

    estilização sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
 import { Header } from './components/Header';
 import { Post } from './Post';
 import './global.css';
+import styles from './App.module.css';
+import { Sidebar } from './components/Sidebar';
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post
-        author="Izadora andorinea"
-        content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas, quisquam eius, aliquid aut ut, fugit eveniet ea voluptates assumenda temporibus accusamus iste enim quidem fugiat. Molestiae rerum officiis aliquid quas?"
-      />
-      <Post
-        author="Cacau gatineo"
-        content="Birman manx lynx yet cheetah. Cornish rex turkish angora malkin. Abyssinian american bobtail or thai or siberian, but himalayan but scottish fold and lynx. Himalayan."
-      />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Izadora andorinea"
+            content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas, quisquam eius, aliquid aut ut, fugit eveniet ea voluptates assumenda temporibus accusamus iste enim quidem fugiat. Molestiae rerum officiis aliquid quas?"
+          />
+          <Post
+            author="Cacau gatineo"
+            content="Birman manx lynx yet cheetah. Cornish rex turkish angora malkin. Abyssinian american bobtail or thai or siberian, but himalayan but scottish fold and lynx. Himalayan."
+          />
+        </main>
+      </div>
     </div>
   );
 }
