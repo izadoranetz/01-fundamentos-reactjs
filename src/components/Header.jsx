d8b063bb5fec12b5e441873c98540d79aff27deb
izadoranetz <izadora.ns@gmail.com>
2022-11-07 16:32:43 -0300

    Componente Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,10 @@
-import styles from './Header.module.css'
+import styles from './Header.module.css';
+import igniteLogo from '../assets/ignite-logo.svg';
 
 export function Header() {
-  return <strong className={styles.header}>Ignite Feed</strong>;
+  return (
+    <header className={styles.header}>
+      <img src={igniteLogo} alt="Logotipo do Ignite" />
+    </header>
+  );
 }
